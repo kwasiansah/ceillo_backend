703123806e5b248b8a58dd01da45c66b921117d9
kwasi_ansah <kwasiansahasare@gmail.com>
2022-08-06 15:02:30 +0000

    feat: code style config

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -108,23 +108,23 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'ceillo.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "ceillo",
-        "USER": "ceillo",
-        "PASSWORD": "ceillo@123",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'ceillo.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "ceillo",
+#         "USER": "ceillo",
+#         "PASSWORD": "ceillo@123",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
